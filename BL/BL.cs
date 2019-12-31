2d2abe7b0ff184bd90070c990c8f56b8eea7bff5
talia_alon@hotmail.com <talia_alon@hotmail.com>
2019-12-31 13:45:49 +0200

    BL

--- BL/BL.cs
+++ BL/BL.cs
@@ -3,10 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BE;
 
 namespace BL
 {
-    public class BL
+    public class BL : IBL
     {
+        public void addGuestRequest(GuestRequest My_GuestRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addHostingUnit(HostingUnit My_HostingUnit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addOrder(Order My_Order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteHostingUnit(long hosting_unit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BankBranch> My_BankBranchList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<GuestRequest> My_guestRequestsList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<HostingUnit> My_HostingUnitList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> My_OrdersList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Requirement_update(GuestRequest My_GuestRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateHostingUnit(HostingUnit My_HostingUnit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrder(Order My_Order)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
